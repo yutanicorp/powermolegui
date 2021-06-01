93ca1f3a5c9ea029420d1cad277884cf04c266ad
Vincent Schouten <inquiry@intoreflection.co>
2021-06-01 23:03:43 +0200

    Add instructions how to execute powermolegui

--- execute_gui.py
+++ execute_gui.py
@@ -6,3 +6,6 @@ from powermolegui.powermolegui import main
 
 if __name__ == '__main__':
     main()
+
+# 1. issue ". powermolegui/.venv/bin/activate" in directory where both packages (gui and lib) reside
+# 2. issue "python execute_gui.py -c /home/vincent/demo_2_hosts_for_mode.json -L debug"
