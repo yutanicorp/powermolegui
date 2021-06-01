a4dc4b4fdc9f07cdd3e822d212f3462144ccefc5
Vincent Schouten <inquiry@intoreflection.co>
2021-06-01 23:02:30 +0200

    Reword list of keywords for PyPi

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=requirements,
     license='MIT',
     zip_safe=False,
-    keywords='''powermole powermolegui ssh proxyjump forwarding tor gui''',
+    keywords='''powermole powermolegui ssh proxyjump forwarding tor gui security''',
     entry_points = {
                    'console_scripts': [
                        # enable this to automatically generate a script in /usr/local/bin called myscript that points to your
